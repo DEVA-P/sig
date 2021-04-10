b21d0cec5d7a4ecfb0aa3eef03b3ccdc89d2b452
DEVA-P <devapoonkl@gmail.com>
2021-04-10 14:33:24 +0530

    file added

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-function fun()
-{ 
-    document.getElementById("in").value="TEXT WILL BE SHOWN BELOW";
-} 
-function WhichButton(event){ 
-        var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-        document.getElementById("color1").style.background=randomColor;
-}
+ 
